08b6a730c4124075b55961e6a27bde0ef5b31285
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-01-18 18:14:17 +0100

    add profiles for a 4-wheeled, skid-steering system

---
+++ models/actions/rock_ugv_nav_profiles.rb
@@ -0,0 +1,21 @@
+require 'models/blueprints/control'
+using_task_library 'skid4_control'
+using_task_library 'odometry'
+
+module Rock
+    module UGVNavProfiles
+        action_library
+
+        syskit_profile 'Skid4' do
+            use Base::Motion2DControlledSystemSrv =>
+                Base::ControlLoop.use('controller' => Skid4Control::SimpleController)
+            use SLAM::OdometrySrv => Odometry::Skid4OdometryTask
+
+            define 'joystick_drive', Base::ControlLoop.use('controller' => Controldev::JoystickTask)
+            define 'local_navigation', Rock::CorridorNavigation::Servoing.use(
+                'pose' => SLAM::OdometrySrv)
+            define 'explore', Rock::CorridorNavigation::Servoing.use(
+                'pose' => SLAM::OdometrySrv)
+        end
+    end
+end
