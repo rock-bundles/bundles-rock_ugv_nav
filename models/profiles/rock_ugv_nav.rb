93e72deef36c9fb1452c6687c77305e7bad36621
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-02-01 16:17:52 +0100

    adapt to the updated profile definition API
    Move the files to profiles/ and into the Rock::UGVNav module. This
    module does not need to be an action library anymore.

--- models/profiles/rock_ugv_nav.rb
+++ models/profiles/rock_ugv_nav.rb
@@ -1,12 +1,11 @@
 require 'models/blueprints/control'
+require 'models/blueprints/corridor_navigation'
 using_task_library 'skid4_control'
 using_task_library 'odometry'
 
 module Rock
-    module UGVNavProfiles
-        action_library
-
-        syskit_profile 'Skid4' do
+    module UGVNav
+        profile 'Skid4' do
             use Base::Motion2DControlledSystemSrv =>
                 Base::ControlLoop.use('controller' => Skid4Control::SimpleController)
             use SLAM::OdometrySrv =>
@@ -15,7 +14,7 @@ module Rock
             define 'joystick_drive', Base::ControlLoop.use('controller' => Controldev::JoystickTask)
             define 'local_navigation', Rock::CorridorNavigation::Servoing.use(
                 'pose' => SLAM::OdometrySrv)
-            define 'explore', Rock::CorridorNavigation::Servoing.use(
+            define 'explore', Rock::CorridorNavigation::Explore.use(
                 'pose' => SLAM::OdometrySrv)
         end
     end
