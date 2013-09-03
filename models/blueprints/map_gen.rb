954861e2a0406ca9d4d65760534bad557b66fbe5
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-09-03 14:28:30 +0200

    moved generic mapgen service definitions to the rock bundle

--- models/blueprints/map_gen.rb
+++ models/blueprints/map_gen.rb
@@ -1,11 +1,6 @@
-module Rock
-    # This module contains the standard map generation functionality in Rock
-    module MapGen
-    end
-end
+require 'rock/models/blueprints/map_gen'
 
 require 'models/blueprints/odometry'
-require 'models/blueprints/map_gen/map_generator_srv'
 require 'models/blueprints/map_gen/local_map'
 require 'models/blueprints/map_gen/eslam'
 require 'models/blueprints/map_gen/traversability_map'
