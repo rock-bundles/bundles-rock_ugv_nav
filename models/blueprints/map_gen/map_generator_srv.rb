a20a63e3cda8f00ac5d5ee5a2e00ae4b405553c7
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-09-03 14:24:32 +0200

    create MapGen::OneShotSrv to mark map generators that don't continuously generate maps
    We need to synchronize on those so that the map is generated *after*
    the receivers are ready to receive it.

--- models/blueprints/map_gen/map_generator_srv.rb
+++ models/blueprints/map_gen/map_generator_srv.rb
@@ -11,5 +11,19 @@ module Rock
         data_service_type 'TraversabilitySrv' do
             provides MapGeneratorSrv
         end
+
+        # Data service that is used to tag map generators that provide a single
+        # map at startup, and then nothing
+        data_service_type 'OneShotSrv'
+        
+        def OneShotSrv.included(other)
+            # Called when the service gets provided by a task or another
+            # service. It marks the task as non-reusable
+            if other <= Syskit::TaskContext
+                other.on :start do |event|
+                    event.task.needs_reconfiguration!
+                end
+            end
+        end
     end
 end
