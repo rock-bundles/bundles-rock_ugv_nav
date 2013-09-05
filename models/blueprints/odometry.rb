7621a1edc17ab4202aef9a036f85d2c8c430538c
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-09-05 17:47:29 +0200

    cleanup handling of the odometry module
     - the generic modeling code is in Odometry::Generic
     - the specializations are in blueprints
     - added specialization for the new Odometry::Skid task

--- models/blueprints/odometry.rb
+++ models/blueprints/odometry.rb
@@ -12,13 +12,27 @@ module SLAM
     data_service_type 'BodyContactStateSrv' do
         output_port 'contact_samples', '/odometry/BodyContactState'
     end
+end
 
+using_task_library 'odometry'
+
+module SLAM
     class Odometry < Syskit::Composition
         add Base::OrientationSrv, :as => 'orientation'
         add OdometrySrv, :as => 'odometry'
         export odometry_child.pose_samples_port
         export odometry_child.pose_delta_samples_port
         provides OdometrySrv, :as => 'odometry'
+
+        specialize odometry_child => ::Odometry::Skid do
+            add Base::JointsControlledSystemSrv, :as => 'joint_status'
+            joint_status_child.connect_to odometry_child
+        end
+
+        specialize odometry_child => ::Odometry::ContactPointTask do
+            add SLAM::BodyContactStateSrv, :as => 'contact_points'
+            contact_points_child.connect_to odometry_child
+        end
     end
 end
 
