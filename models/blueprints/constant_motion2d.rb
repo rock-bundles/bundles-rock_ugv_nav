6f7435c15e48fd654512eb176b54fc4c9361f4c5
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-11-05 22:10:55 +0100

    move ConstantMovement to ConstantMotion2D to make the name more explicit, add test file

--- models/blueprints/constant_motion2d.rb
+++ models/blueprints/constant_motion2d.rb
@@ -3,17 +3,16 @@ module Rock
     module UGVNav
         # Generates a constant Motion2DCommand and outputs it to a
         # Motion2DControlledSystemSrv component
-        class ConstantMovement < Syskit::Composition
+        class ConstantMotion2D < Syskit::Composition
             argument :translation, :default => 0
             argument :rotation, :default => 0
             add Base::Motion2DControlledSystemSrv, :as => 'system'
 
-            on :start do
-                @writer = system_child.command_in_port.writer
-            end
-
-            poll do
-                writer.write(:translation => translation, :rotation => rotation)
+            script do
+                cmd_w = system_child.command_in_port.writer
+                poll do
+                    cmd_w.write(:translation => translation, :rotation => rotation)
+                end
             end
         end
     end
