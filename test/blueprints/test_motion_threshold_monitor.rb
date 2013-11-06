9e9b25badc3c3994f64932cd8e929f9097550c23
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-11-06 15:54:23 +0100

    fix tests for MotionThresholdMonitor
    The arguments got renamed

--- test/blueprints/test_motion_threshold_monitor.rb
+++ test/blueprints/test_motion_threshold_monitor.rb
@@ -28,38 +28,38 @@ describe Rock::UGVNav::MotionThresholdMonitor do
     end
 
     it "initializes the initial and current position/heading with the first sample" do
-        cmp, port = deploy_test_network(:linear_threshold => 10, :angular_threshold => nil)
+        cmp, port = deploy_test_network(:translation_threshold => 10, :rotation_threshold => nil)
         assert_equal rbs.position, cmp.initial_position, rbs.position
         assert_equal rbs.position, cmp.current_position, rbs.position
         assert_equal 0, cmp.initial_heading
         assert_equal 0, cmp.current_heading
     end
 
-    it "emits triggered if the initial pose and next pose cross the linear threshold" do
-        cmp, port = deploy_test_network(:linear_threshold => 10, :angular_threshold => nil)
+    it "emits triggered if the initial pose and next pose cross the translation threshold" do
+        cmp, port = deploy_test_network(:translation_threshold => 10, :rotation_threshold => nil)
         rbs.position = Eigen::Vector3.new(-10, -10, 0)
         assert_event_emission cmp.triggered_event do
             port.write rbs
         end
     end
 
-    it "ignores heading if the angular threshold is nil" do
-        cmp, port = deploy_test_network(:linear_threshold => 10, :angular_threshold => nil)
+    it "ignores heading if the rotation threshold is nil" do
+        cmp, port = deploy_test_network(:translation_threshold => 10, :rotation_threshold => nil)
         rbs.orientation = Eigen::Quaternion.from_angle_axis(2, Eigen::Vector3.UnitZ)
         process_events
         assert !cmp.triggered?
     end
 
-    it "emits triggered if the initial heading and next heading cross the angular threshold" do
-        cmp, port = deploy_test_network(:linear_threshold => nil, :angular_threshold => 1)
+    it "emits triggered if the initial heading and next heading cross the rotation threshold" do
+        cmp, port = deploy_test_network(:translation_threshold => nil, :rotation_threshold => 1)
         rbs.orientation = Eigen::Quaternion.from_angle_axis(-1.1, Eigen::Vector3.UnitZ)
         assert_event_emission cmp.triggered_event do
             port.write rbs
         end
     end
 
-    it "ignores position if the linear threshold is nil" do
-        cmp, port = deploy_test_network(:linear_threshold => nil, :angular_threshold => 1)
+    it "ignores position if the translation threshold is nil" do
+        cmp, port = deploy_test_network(:translation_threshold => nil, :rotation_threshold => 1)
         rbs.position = Eigen::Vector3.new(-5, -5, 0)
         process_events
         assert !cmp.triggered?
