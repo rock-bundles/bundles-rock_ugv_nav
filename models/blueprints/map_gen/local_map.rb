823569a3832369a08196b83f7801eeb1e20306d2
Raul Dominguez <raul.dominguez@dfki.de>
2014-09-24 11:28:22 +0200

    Adapts the map generation, so to match the current navigation tasks.

--- models/blueprints/map_gen/local_map.rb
+++ models/blueprints/map_gen/local_map.rb
@@ -1,14 +1,14 @@
 require 'models/blueprints/pose'
 require 'models/blueprints/sensors'
-using_task_library 'corridor_navigation'
+using_task_library 'local_mapper'
 module Rock
     module MapGen
         class LocalMap < Syskit::Composition
             add Base::LaserRangeFinderSrv, :as => 'laser'
             add Base::PoseSrv, :as => 'pose'
-            add(CorridorNavigation::ServoingTask, :as => 'generator').
-                with_arguments(:initial_heading => nil)
-            #pose_child.pose_samples_port.connect_to generator_child.odometry_samples_port
+            add(LocalMapper::Task, :as => 'generator')
+            #    with_arguments(:initial_heading => nil)
+            pose_child.pose_samples_port.connect_to generator_child
             laser_child.connect_to generator_child.scan_samples_port
         end
     end
--- models/blueprints/map_gen/traversability_map.rb
+++ models/blueprints/map_gen/traversability_map.rb
@@ -1,13 +1,13 @@
 require 'models/blueprints/map_gen/map_generator_srv'
 require 'models/blueprints/map_gen/pipeline_base'
-using_task_library 'corridor_planner'
+using_task_library 'traversability'
 module Rock
     module MapGen
         class TraversabilityMap < PipelineBase
             event :trigger
 
             overload map_source_child, MLSSrv
-            add_main_task CorridorPlanner::Traversability, :as => 'traversability_builder'
+            add_main_task Traversability::Grassfire, :as => 'traversability_builder'
             export traversability_builder_child.traversability_map_port
             map_source_child.map_port.connect_to traversability_builder_child.mls_map_port
             provides TraversabilitySrv, :as => 'traversability_map'
