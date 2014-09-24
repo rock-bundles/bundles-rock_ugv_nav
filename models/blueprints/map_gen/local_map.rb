da723f02bca551794b705a989db780801855ba30
Raul Dominguez <raul.dominguez@dfki.de>
2014-09-24 13:21:04 +0200

    No pose is involved in the local map composition

--- models/blueprints/map_gen/local_map.rb
+++ models/blueprints/map_gen/local_map.rb
@@ -1,14 +1,14 @@
-require 'models/blueprints/pose'
+#require 'models/blueprints/pose'
 require 'models/blueprints/sensors'
 using_task_library 'local_mapper'
 module Rock
     module MapGen
         class LocalMap < Syskit::Composition
             add Base::LaserRangeFinderSrv, :as => 'laser'
-            add Base::PoseSrv, :as => 'pose'
+            #add Base::PoseSrv, :as => 'pose'
             add(LocalMapper::Task, :as => 'generator')
             #    with_arguments(:initial_heading => nil)
-            pose_child.pose_samples_port.connect_to generator_child
+            #pose_child.pose_samples_port.connect_to generator_child
             laser_child.connect_to generator_child.scan_samples_port
         end
     end
